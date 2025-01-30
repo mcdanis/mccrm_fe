26695e366f89b32d87fd28534143525dc74f280c
mcdsaputra <dsaputra@salespond.com>
2025-01-30 09:03:34 +0700

    fix font

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,11 @@ const nextConfig: NextConfig = {
   env: {
     JWT: process.env.JWT,
     URL: process.env.URL,
-  }
+  },
+  font: [{
+    name: 'Inter',
+    url: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap'
+  }]
 };
 
 export default nextConfig;
