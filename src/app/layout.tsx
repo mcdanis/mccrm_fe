53efa07d1188c013a1e4afbfc8c49c6710409b05
mcdsaputra <dsaputra@salespond.com>
2025-01-30 11:03:12 +0700

    fix font 4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
-
-const inter = Inter({ subsets: ["latin"], weight: ["400", "700"] });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
